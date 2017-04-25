83832471f4b5984e310f66ece3abc257c2f2b094
Drew Nickerson <dnick13@gmail.com>
2017-04-25 15:05:53 -0400

    Formatting Changes

--- server.js
+++ server.js
@@ -24,23 +24,18 @@ let port = process.env.PORT;
 // Routing
 // ====================
 
-// let router = express.Router();
-
-//
-// Base Route
-//
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
+  
   request('http://localhost:4567/', function (error, response, body) {
     console.log('body:', body); // Print the HTML for the Google homepage.
-});
+  });
 });
 
-// // ====================
-// // Register Routes
-// // ====================
-//
-// app.use('/', router);
+// ====================
+// Register Routes
+// ====================
+
 app.use("/", express.static(__dirname + '/'));
 
 // ====================
