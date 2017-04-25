3c7740636cbe4aa90fe6044b365a3c9d2fe7d63c
Drew Nickerson <dnick13@gmail.com>
2017-04-25 15:00:22 -0400

    Added ability to test notification with button

--- scripts/helper/twitter.js
+++ scripts/helper/twitter.js
@@ -1,6 +1,49 @@
+// ============================
+// Register the Service Worker
+// ============================
+
 navigator.serviceWorker.register('./sw.js');
 
+// ============================
+// Instant Gratification/ Testing
+// ============================
+
 var button = document.getElementById('test-notification');
+
+button.addEventListener('click', function() {
+
+  Notification.requestPermission( () => {
+
+    navigator.serviceWorker.ready
+    .then( (registration) => {
+
+      function httpGet(theUrl) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+        return xmlHttp.responseText;
+      }
+
+      var newBody = httpGet('http://localhost:4567/')
+
+      var options = {
+        body: "",
+        icon: 'https://www.gdnmobilelab.com/workshop/icon.png',
+        badge: 'https://www.gdnmobilelab.com/workshop/badge.png',
+        silent: true
+      };
+
+      registration.active.postMessage({
+        title: newBody,
+        options: options
+      });
+    })
+  })
+});
+
+// ============================
+// 45 minute interval pushes
+// ============================
 
 window.setInterval( () => {
 // button.addEventListener('click', function() {
